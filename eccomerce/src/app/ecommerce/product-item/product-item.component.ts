3936c40b522c6e9555332d17855b85c44e6835e7
bashfulmonkey <rearick.seth@gmail.com>
2024-08-20 15:48:29 -0400

    Create html for product item and add item photo.

--- eccomerce/src/app/ecommerce/product-item/product-item.component.ts
+++ eccomerce/src/app/ecommerce/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-product-item',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.css'
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+    public product!: Product;
 
+    constructor () {}
+
+    ngOnInit(): void {
+        this.product = new Product('The Bonny Lane Club', 5.25,
+                                   "assets/images/bonny-lane-club-book.jpg", true);
+    }
 }
